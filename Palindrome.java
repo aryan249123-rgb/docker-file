49bf21629fe8108742cff757f175754841f76402
aryan249123-rgb <aryan249123@gmail.com>
2026-03-28 17:21:27 +0530

    2nd command

--- Palindrome.java
+++ Palindrome.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String str = "madam";
         String rev = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
